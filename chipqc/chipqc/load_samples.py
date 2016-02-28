b6c6ea064f173169be4e3e7bf713059ff94a9d28
Matthias Haimel <mh719@cam.ac.uk>
2016-02-28 15:38:05 +0000

    load: allow annotation files only to be loaded

--- chipqc/chipqc/load_samples.py
+++ chipqc/chipqc/load_samples.py
@@ -9,10 +9,11 @@ def getHelpInfo():
     return 'load sample information'
 
 def addArguments(parser):
-    parser.add_argument('-f','--file-list',type=str,dest='fl_file',required=True,help="Load / Update a tab separated file (incl. header) with three (or more) columns: <id> <file> <annotation> [<annotations>]")
+    parser.add_argument('-f','--file-list',type=str,dest='fl_file',required=False,help="Load / Update a tab separated file (incl. header) with three (or more) columns: <id> <file> <annotation> [<annotations>]")
+    parser.add_argument('-a','--annotate',type=str,dest='at_file',required=False,help="Update entry with a tab separated file or annotations (incl. header) with one (or more) columns: <id> [<annotations>]")
 #    parser.add_argument('-F','--files-add',type=str,dest='fa_file',required=False,help="Add (and ignore row if exists) a tab separated file (incl. header) with three (or more) columns: <id> <file> <annotation> [<annotations>]")
 
-def parseCsv(file):
+def parseCsv(file, annotOffset=2):
     exp = list()
 
     with open(file,"r") as tsvfile:
@@ -25,7 +26,7 @@ def parseCsv(file):
                 entry = ()
                 # Header line
                 for hidx,key in enumerate(line):
-                    if(hidx > 1):# Only annotations
+                    if(hidx >= annotOffset):# Only annotations
                         entry += (key,)
                 exp.append(entry)
             else:
@@ -132,13 +133,37 @@ def addFile(db, file): # NOT USED
     print ("Prepared correlations: %s new from %s" % (newCnt,corrCnt))
     print("Done")
 
+def annotateFile(db, file):
+    print("Add annotation")
+    dataFile = os.path.abspath(file)
+    print "Loading file %s " %(dataFile,)
+    exp = parseCsv(file=dataFile, annotOffset=1)
+    print("Entries found in file: %s " % (len(exp)-1))
+    ext_dict = { ext:did for did,ext,f in db.getFiles() }
+
+    new_annot = [row for idx,row in enumerate(exp) if idx == 0 or row[0] in ext_dict.keys() ]
+    print("Loading %s rows of annotations into DB ... " % (len(new_annot)-1,))
+
+    annot_tupels = tuple()
+    for idx,row in enumerate(new_annot):
+        if idx == 0:
+            annot_desc = row
+            print(annot_desc)
+        elif row[0] in ext_dict.keys():
+            print (row[0])
+            qid = ext_dict[row[0]]
+            for aidx,ae in enumerate(annot_desc):
+                annot_tupels += ((qid,ae,row[1+aidx]),)
+    print(annot_tupels)
+    db.addAnnotationAll(annot_tupels)
+
 def run(parser,args):
     db_file=args.db_file
     db = chipqc_db.ChipQcDbSqlite(path=db_file)
 
     if 'fl_file' in args and args.fl_file is not None:
         load(db,args.fl_file)
-    # elif 'fa_file' in args and args.fa_file is not None:
-    #     addFile(db,args.fa_file)
+    elif 'at_file' in args and args.at_file is not None:
+         annotateFile(db,args.at_file)
     else:
         parser.print_help()
